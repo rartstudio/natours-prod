3a9ca3ae56cf10839c6e56606ea094a0966f0278
rartstudio <58383924+rartstudio@users.noreply.github.com>
2020-11-18 20:57:55 +0700

    set koneksi ke mongo berhasil

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
+
+//save connection string and replace password text to env database password
+const DB = process.env.DATABASE.replace('<PASSWORD>',process.env.PASSWORD);
+
+//options to deal with some deprecation warnings (recommended)
+//if connection success it will return a object to the console
+//if success comment in con.connections and change con to ()
+mongoose.connect(DB, {
+  useUnifiedTopology: true,
+	useNewUrlParser: true,
+	useCreateIndex: true,
+  useFindAndModify: false,
+}).then(con => {
+	console.log(con.connections);
+	console.log('DB CONNECTION SUCCESFULY');
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
